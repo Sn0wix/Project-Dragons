03c8499e588d6037d14a248701421eed63176d0c
Sn0wix_ <sn0wix@proton.me>
2025-08-17 16:43:31 +0200

    init

---
+++ src/client/java/net/sn0wix_/projectdragons/ProjectDragonsClient.java
@@ -0,0 +1,10 @@
+package net.sn0wix_.projectdragons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectDragonsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
